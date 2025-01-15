90583956100cfc5cad972a47442d148beb190b3c
Jessica staggs <deida0902@hotmail.com>
2025-01-14 21:51:10 -0500

    clear input field, add <a>tag to myLeads and refractor code to template string

--- script.js
+++ script.js
@@ -7,14 +7,20 @@ console.log(ulEl)
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
-    console.log(myLeads)
+    inputEl.value = ""
     renderLeads()
 })
 
 function renderLeads () {
-let listItems = ""
-for (let i =0; i < myLeads.length; i++){
-    listItems += "<li>" + myLeads[i] + "</li>"
-}
-ulEl.innerHTML = listItems
+    let listItems = ""
+    for (let i =0; i < myLeads.length; i++){
+        listItems += `
+            <li>
+                <a target='_blank' href='${myLeads[i]}'>
+                    ${myLeads[i]}
+                </a>
+            </li>
+    `        
+    }
+    ulEl.innerHTML = listItems
 }
