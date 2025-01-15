14d08a17933b2fa9c439d299e382944827740df3
Jessica staggs <deida0902@hotmail.com>
2025-01-14 21:14:54 -0500

    render leads in ul via .innerHTML & create render function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let myLeads = ["www.awesomeleads.com", "www.epiclead.com", "www.greatlead.com"]
+let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
@@ -8,8 +8,13 @@ console.log(ulEl)
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
+    renderLeads()
 })
 
+function renderLeads () {
+let listItems = ""
 for (let i =0; i < myLeads.length; i++){
-    console.log(myLeads[i])
+    listItems += "<li>" + myLeads[i] + "</li>"
+}
+ulEl.innerHTML = listItems
 }
